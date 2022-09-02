7801f9215d1d522ac1afbe91481fa7908a691ded
JzChannel03 <jzchannel03@outlook.com>
2022-09-02 13:34:37 -0400

    Create GifAppExpert component

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import GifExpertApp from "./components/GifAppExpert";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <App />
+    <GifExpertApp/>
   </React.StrictMode>
 );
+
