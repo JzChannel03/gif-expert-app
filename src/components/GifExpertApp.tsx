d8d14d3c052bba58ee5d730bdbab496a9ad4e6a7
JzChannel03 <jzchannel03@outlook.com>
2022-09-03 14:11:36 -0400

    -GifExpertApp component Complete -Add Sass compatibility

---
+++ src/components/GifExpertApp.tsx
@@ -0,0 +1,42 @@
+import React, {useState} from "react";
+
+const GifExpertApp = () => {
+
+    const [Categories, setCategories] = useState(['One Punch', 'Samurai X', 'Dragon Ball', 'Sonic X']);
+
+    const link = (e: React.SyntheticEvent) => {
+        window.location.href = `https://google.com/search?q=${(e.target as HTMLUListElement).textContent}`
+    }
+
+    const handleDelete = (e: React.SyntheticEvent) => {
+        setCategories((value) => {
+            if (e.isTrusted){
+                value.pop();
+                e.isTrusted = false;
+            }
+            return [...value];
+        });
+    }
+
+
+    return (
+        <div>
+            <h2>GifExpertApp</h2>
+            <hr/>
+            <h3>Más buscados: </h3>
+            <ul id={"listMoreSearch"}>
+                {Categories.map((value) => {
+                    return (
+                        <li key={value} onClick={link}>{value}</li>
+                    );
+                })}
+            </ul>
+            {Categories.length && <button onClick={handleDelete}>Borrar último elemento</button>}
+
+        </div>
+    )
+}
+
+
+
+export default GifExpertApp;
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import GifExpertApp from "./components/GifAppExpert";
+import GifExpertApp from "./components/GifExpertApp";
+import './styles/index.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
 root.render(
-  <React.StrictMode>
-    <GifExpertApp/>
-  </React.StrictMode>
+    <React.StrictMode>
+        <GifExpertApp/>
+    </React.StrictMode>
+
 );
 
