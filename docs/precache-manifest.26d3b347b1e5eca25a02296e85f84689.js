1ab12773ea0a9c47a7e062328c9c35b2fca628cc
Jose Ventura <jzchannel03@outlook.com>
2022-09-11 14:14:08 -0400

    Added docs

---
+++ docs/precache-manifest.26d3b347b1e5eca25a02296e85f84689.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4d04411748ab4da380a2",
+    "url": "/static/css/main.82398d7e.chunk.css"
+  },
+  {
+    "revision": "4d04411748ab4da380a2",
+    "url": "/static/js/main.4d044117.chunk.js"
+  },
+  {
+    "revision": "9e2ec8e2eb850665d042",
+    "url": "/static/js/1.9e2ec8e2.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6ab168b07ba7c89f5cb7c0e37e25f79a",
+    "url": "/index.html"
+  }
+];
